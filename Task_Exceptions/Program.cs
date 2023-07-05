2b98dd8572100a7cac87244359a356e6d7390937
HuseynFarzali <huseynyf@code.edu.az>
2023-07-05 06:29:02 -0700

    Add project files.

---
+++ Task_Exceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_Exceptions
+{
+    public static class EmailValidator
+    {
+        public static bool Validate(string email, string validExtension = "@mail.com")
+        {
+            int length = email.Length;
+            if (length < 10) throw new ShortEmailException(
+                message: "Invalid string length",
+                description: $"Provided string does not have a valid length (--> {length}) for an email. ");
+
+            if (!email.EndsWith(validExtension)) throw new NotAnEmailAddress(
+                message: "Invalid email",
+                description: $"Provided string does not conincide with given mail template: ...{validExtension}");
+
+            return true;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the email: ");
+                string email = Console.ReadLine();
+
+                bool success = EmailValidator.Validate(email);
+
+                if (success) { Console.WriteLine("Inputted email is valid"); }
+            }
+
+            catch (ShortEmailException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (NotAnEmailAddress ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Task_Exceptions/ShortEmailException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Exceptions
+{
+    public class ShortEmailException : Exception
+    {
+        public override string? Message { get; } = "[no message provided]";
+        public string? Description { get; } = "[no description provided]";
+
+        public ShortEmailException() : base() { }
+        public ShortEmailException(string message, string description)
+        {
+            Message = message;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"Exception: {this.GetType().ToString()} --> {this.Message}\n" +
+                $"Exception Description: {this.Description}\n" +
+                $"Exception StackTrace:\n{this.StackTrace}"; ;
+        }
+    }
+}
